dc43572e8d548de232032f3ef201c729d18e2d60
ADAMU M. M <muhdadamu0008@gmail.com>
2021-11-23 22:49:03 +0100

    color setup

--- includes/header.php
+++ includes/header.php
@@ -6,8 +6,77 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content />
         <meta name="author" content />
-        <title>Home-TechBarik</title>
+        <title>Home-Acadamics</title>
         <link href="css/styles.css" rel="stylesheet" />
         <link rel="icon" type="image/x-icon" href="img/favicon.png" />
+<style type="text/css">
+        .green{
+  background: rgba(137, 214, 92, 0.548);
+  color: #fff !important;
+}  
+.bg-gradient-green-to-cyan{
+         background-color: #14b182;
+  background-image: linear-gradient(135deg, #14b182 0%, rgba(137, 214, 92, 0.548) 100%);
+
+}
+.bg-green-ly{
+  background-color: #14bb82; !important;
+}
+
+.text{
+  color: #14b182;
+}
+.border{
+  border: 1px solid #14b182 !important; 
+  
+ }
+ .btn-green{
+  color: #fff;
+  background-color: #14b182;;
+  border-color: #14b182;
+}
+.btn-green:hover {
+  color: #fff;
+  background-color: #14c266;;
+  border-color: #14a182;;
+}
+.btn-green:not(:disabled):not(.disabled):active, .btn-green:not(:disabled):not(.disabled).active, .show > .btn-green.dropdown-toggle {
+    color: #fff;
+  background-color: #14b182;
+  border-color: #14b182;;
+
+
+}
+.text-header{
+  width:50%;
+  margin:auto;
+  border-bottom:1px #14b182 solid;
+}
+/* img{
+    margin-top:-50px;
+  } */
+
+.fa{
+    font-size:25px;
+  }
+
+.card-Author:hover{
+    background: rgba(137, 214, 92, 0.548);
+    color:#fff; }
+
+.fa{
+      color:#14b182;
+    }  
+div ul li a{
+  color: #14b182 !important;
+}
+.fa-facebook{
+  color: blue;
+}
+.fa-twitter{
+  color: #099;
+}
+
+</style>
     </head>
     <body>
